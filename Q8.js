5e9c2b56a8440cb1d96e3782364ce47a520d9503
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:07:12 -0800

    Question 8

---
+++ Q8.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Program#8: Write a program that checks if the givin year is leap year or not
+function leapYear(GivenYear) {
+    if (GivenYear % 4 === 0) {
+        console.log(`The given Year ${GivenYear} is a leap year`);
+    }
+    else {
+        console.log(`The given Year ${GivenYear} is a leap year`);
+    }
+}
+leapYear(2000);
---
+++ Q8.ts
@@ -0,0 +1,10 @@
+// Program#8: Write a program that checks if the givin year is leap year or not
+function leapYear(GivenYear: number) {
+  if (GivenYear % 4 === 0) {
+    console.log(`The given Year ${GivenYear} is a leap year`);
+  } else {
+    console.log(`The given Year ${GivenYear} is a leap year`);
+  }
+}
+
+leapYear(2000);

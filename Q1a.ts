9310bd602af5fd7933c228c2783f876bb089d330
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:05:17 -0800

    Question 1

---
+++ Q1a.ts
@@ -0,0 +1,6 @@
+// Program#1: Write a program to convert the tempreture from Celsius to Fahrenheit
+function celsiusToFahrenheit(c: number) {
+  let f = c * 1.8 + 32;
+  console.log(`The tempreture of ${c} celsius is ${f} Fahrenheit`);
+}
+celsiusToFahrenheit(30);

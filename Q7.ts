8afdc692bb11473da3c7c36c2310bf795a78079c
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:06:57 -0800

    Question 7

---
+++ Q7.ts
@@ -0,0 +1,16 @@
+// Program#7: Write a program that check if the givin number is divisible by 3 or 5 or both or not divisible by anyone
+function numberDivisible(num: number) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log(`Your number ${num} is divisible both of 3 and 5`);
+  } else if (num % 3 === 0) {
+    console.log(`Your number ${num} is divisible  3 `);
+  } else if (num % 5 === 0) {
+    console.log(`Your number ${num} is divisible  5 `);
+  } else {
+    console.log(
+      `Your number ${num} is nor divisible by 3 and not divisible by 5 `
+    );
+  }
+}
+
+numberDivisible(14);
